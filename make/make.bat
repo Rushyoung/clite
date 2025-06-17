ee07506e699cdb0035b09d48150f1d5091fa3351
Wu Junkai <wu.junkai@qq.com>
2025-06-17 23:19:59 +0800

    chore: 更新make.bat脚本，集成TCC和GCC编译器，优化输出信息

--- make/make.bat
+++ make/make.bat
@@ -3,20 +3,33 @@
 
 for /f "delims=#" %%i in ('prompt #$E#^&echo on^&for %%a in ^(1^) do rem') do set esck=%%i
 set "e=%esck%[38;5;196m"
-set "g=%esck%[38;5;46m"
-set "y=%esck%[38;5;226m"
+set "i=%esck%[38;5;46m"
+set "w=%esck%[38;5;226m"
 set "R=%esck%[m"
+
+tcc --version >nul 2>&1
+if errorlevel 1 (
+    echo [%w%Warning%R%]: TCC ^(Tiny C Compiler^) is not installed or not found in PATH.
+) else (
+    echo [%i%Info%R%]: TCC ^(Tiny C Compiler^) is installed.
+    set "cc=tcc"
+    set "CFLAGS=-O2"
+    goto:compile
+)
 
 gcc --version >nul 2>&1
 if errorlevel 1 (
-    echo [%e%Error%R%]: GCC ^(GNU Compiler Collection^) is not installed or not found in PATH.
+    echo [%e%Error%R%]: gcc ^(GNU Compiler Collection^) is not installed or not found in PATH.
     exit /b 1
-)
-
+) 
+echo [%i%Info%R%]: gcc ^(GNU Compiler Collection^) is installed.
+set "cc=gcc"
 set "CFLAGS=-Wall -Wextra -Wno-int-conversion -Wno-switch -std=c11 -O2"
 
+
+:compile
 if not exist main.c (
-    echo [%y%Info%R%]: main.c not found, checking parent directory...
+    echo [%w%Warning%R%]: main.c not found, checking parent directory...
     cd ..
 )
 
@@ -28,21 +41,21 @@ if not exist main.c (
 md build 2> nul | del /Q build\*
 
 for %%i in (src/*.c) do (
-    echo [%g%Info%R%]: Compiling %%~nxi...
-    gcc -c -o build\%%~ni.o src/%%i -Iinclude %CFLAGS%
+    echo [%i%Info%R%]: Compiling %%~nxi...
+    %cc% -c -o build\%%~ni.o src/%%i -Iinclude %CFLAGS%
     if errorlevel 1 (
         echo [%e%Error%R%]: Compilation failed for %%~nxi.
         exit /b 1
     )
 )
 
-echo [%g%Info%R%]: Compiling main.c...
-gcc -o build/clite.exe build\*.o main.c -Iinclude %CFLAGS%
+echo [%i%Info%R%]: Compiling main.c...
+%cc% -o build/clite.exe build\*.o main.c -Iinclude %CFLAGS%
 
 if errorlevel 1 (
     echo [%e%Error%R%]: Compilation failed for main.c.
     exit /b 1
 )
 
-echo [%g%Info%R%]: linking object files...
-echo [%g%Info%R%]: build completed successfully!
+echo [%i%Info%R%]: linking object files...
+echo [%i%Info%R%]: build completed successfully!
