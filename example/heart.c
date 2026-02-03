23b38f6b29be6ec4343382876b39f7bd76bc6aee
Wu Junkai <wu.junkai@qq.com>
2026-02-03 14:54:26 +0800

    test: Simplify example heart rendering loop
    Declare loop variables inline, remove temporary variables, and use a
    ternary to print '*' or space instead of '.'

--- example/heart.c
+++ example/heart.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    char output;
-    float x, y;
-    float a, ans;
-    for (y = 1.5; y > -1.5; y = y - 0.1) {
-        for (x = -1.5; x < 1.5; x = x + 0.05) {
-            a = x * x + y * y - 1.0;
-            ans = a * a * a - x * x * y * y * y;
-            if(ans <= 0.0){
-                output = '*';
-            } else {
-                output = '.';
-            }
-            printf("%c", output);
+    for (float y = 1.5; y > -1.5; y = y - 0.1) {
+        for (float x = -1.5; x < 1.5; x = x + 0.05) {
+            float a = x * x + y * y - 1.0;
+            printf("%c", a * a * a - x * x * y * y * y <= 0.0 ? '*' : ' ');
         }
         printf("\n");
     }
